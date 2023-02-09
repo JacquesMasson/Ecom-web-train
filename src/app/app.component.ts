7bc05026941ec74e9e17152d3b0c582109680063
Jacques Masson <jacques.masson@epita.fr>
2023-02-09 18:07:38 +0100

    beginning website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentInit{
   title = 'Ecom-web-train';
+
+  ngAfterContentInit(): void {
+  }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateHeaderComponent } from './components/template-header/template-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateHeaderComponent
   ],
   imports: [
     BrowserModule,
